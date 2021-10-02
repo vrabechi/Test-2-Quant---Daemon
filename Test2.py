bf9f184b6f7c13e15e4c19c1032269559e2f5c0f
vrabechi <91572472+vrabechi@users.noreply.github.com>
2021-10-01 21:42:26 -0300

    Add files via upload

--- Test2.py
+++ Test2.py
@@ -3,7 +3,6 @@ import statistics
 from dateutil.relativedelta import relativedelta
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from pandas_datareader import data as wb
 from scipy.optimize import minimize
 from scipy.stats import norm
@@ -23,7 +22,10 @@ for a in assets:
 #treating stock data to get returns and covariance matrix of returns
 pf_returns=stock_prices.pct_change()
 
-#funcion returns assets' risk weight based on portfolio weights and covariance matrix
+#function returns assets' risk weight based on portfolio weights and covariance matrix
+#this approach to the risk contribution is derived from the following:
+#https://people.umass.edu/~kazemi/An%20Introduction%20to%20Risk%20Parity.pdf
+#with this approach, the weights calculation is an iterative process
 def risk_allocation(w, cov_matrix):
     #treat variables as a matrix to use numpy operators
     cov=np.matrix(cov_matrix)
@@ -32,7 +34,7 @@ def risk_allocation(w, cov_matrix):
     #calculate portfolio variance
     portfolio_std=np.sqrt(w*(cov*w.T))
 
-    #risk contribution of each asset
+    #risk contribution of each asset based on its volatility and its covariance with respect to the other assets
     asset_risk_allocation=np.multiply(w.T,(cov*w.T)/portfolio_std)
 
     #returns assets' risk weight
@@ -87,15 +89,17 @@ cov_matrix_0=pf_returns_0.cov()
 #set the constraints and inputs for the later calculation of portfolio weights with the function "minimize"
 #total weight must equal 1
 constraints={'type': 'eq', 'fun': lambda x: sum(x)-1}
+
 #the objective is equal risk weight
 target_risk_w=[0.1] * 10
+
 #initial guess of portfolio composition for iteration
 w_0=[0.1] * 10
-
 
 #use previously defined functions to calculate initial weights of portfolio
 weights_calculation= minimize(cont_error, w_0, args=[target_risk_w, cov_matrix_0], method='SLSQP', constraints=constraints)
 initial_weights=weights_calculation.x
+
 #print initial portfolio composition
 print('The initial portfolio composition is:')
 i=0
@@ -104,19 +108,21 @@ for asset in assets:
     i+=1
 
 #now we will go through the holding period day by day, calculating the updated... 
-#portfolio composition, and rebalancing once per month
-current_month=initial_month
+#portfolio composition, and rebalancing once per month 
+
+#last day of holding period
 end_date=date(2021,8,31)
+
+#some variables need to be initialized
+current_month=initial_month
 current_weights=np.matrix(initial_weights)
 portfolio_return=[]
 buy_list=[]
 sell_list=[]
 cumulative_portfolio_return=1
-cumulative_portfolio_return_plot=[]
 rebalancing_count=1
 
 for day in pf_returns.loc[initial_date:end_date].index:
-#while current_date <= end_date:
 
     #return of individual stocks on current day
     current_returns=pf_returns.loc[day:day].to_numpy()[0,:]
@@ -124,8 +130,7 @@ for day in pf_returns.loc[initial_date:end_date].index:
     #return of portfolio
     portfolio_return.append(sum(np.dot(current_weights,current_returns.T))[0,0])
     cumulative_portfolio_return=cumulative_portfolio_return*(1+portfolio_return[-1])
-    cumulative_portfolio_return_plot.append(cumulative_portfolio_return)
-    
+
     #adjust weights for changes in stock prices and total portfolio return
     current_weights=np.multiply(current_weights,current_returns+1)/(1+portfolio_return[-1])
 
@@ -140,7 +145,8 @@ for day in pf_returns.loc[initial_date:end_date].index:
         #recalculating weights based on data of the last 21 days
         current_weights_rebal=rebal(day, pf_returns, previous_days, target_risk_w, w_0)
 
-        #calculating difference between equal risk weights and current weights, in order to fnid the necessary trades for rebal 
+        #calculating difference between equal risk weights and current weights, in order...
+        #to find the necessary trades for rebal 
         weight_diff=current_weights_rebal-current_weights
 
         #assigning each asset to buy or sell in accordance with the weight differentials 
@@ -172,15 +178,27 @@ for day in pf_returns.loc[initial_date:end_date].index:
 #https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yieldYear&year=2021
 rf=0.02 #%
 
-#calcularing portfolio's return, volatility and sharpe based on daily returns
+#calculating portfolio's return and daily volatility
 cumulative_portfolio_return=round((cumulative_portfolio_return-1)*100,2)
-portfolio_std=round(statistics.stdev(portfolio_return)*100,2)
-sharpe=round((cumulative_portfolio_return-rf)/portfolio_std,2)
-print('\nPortfolio total holding period return: ' + str(cumulative_portfolio_return) + '%')
-print('Portfolio volatility: ' + str(portfolio_std) + '%')
-print('Portfolio\'s Sharpe Index: ' + str(sharpe))
+portfolio_daily_std=round(statistics.stdev(portfolio_return)*100,2)
 
-#calculating VaR for daily return, with a 5% probaility
-VaR=round((statistics.mean(portfolio_return)*100-norm.ppf(0.95)*portfolio_std),2)
+print('\nPortfolio total holding period return: ' + str(cumulative_portfolio_return) + '%')
+
+#number of trading sessions
+nts=len(portfolio_return)
+
+#adjust volatility for the three months holding period
+portfolio_std=portfolio_daily_std*np.sqrt(nts)
+
+print('\nPortfolio holding period volatility: ' + str(round(portfolio_std,2)) + '%')
+
+#calculating VaR for daily return, with a 5% probability of abnormal losses
+#this approach assumes normal return distribution
+VaR=round((statistics.mean(portfolio_return)*100-norm.ppf(0.95)*portfolio_daily_std),2)
 print('Value at Risk: There is a 5% probablity that the portfolio value will decline at least ' + str(-VaR) +'% on a given day')
 
+#sharpe calculation
+sharpe=round((cumulative_portfolio_return-rf)/portfolio_std,2)
+
+#print previously calculated sharpe
+print('\nPortfolio\'s Sharpe Index: ' + str(sharpe))
